721a7b50b988a1d616903eab52b8006ae1e88c2c
nurulabedinashraf@gmail.com <nurulabedinashraf@gmail.com>
2022-10-20 02:17:14 -0500

    added BuildInSelectionSort.java

---
+++ BuildInSelectionSort.java
@@ -0,0 +1,10 @@
+
+import java.util.Arrays;
+
+public class BuildInSelectionSort {
+    public static void main(String[] args){
+        double[] number = {6,0,4,4, 1,9,2,9,3,4,3,5};
+        Arrays.sort(number);
+        System.out.println(Arrays.toString(number));
+    }
+}
